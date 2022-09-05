309622cf70cc4516bcd63dc746ae774ee57d4c89
Kramerale <aliceshoolz96@gmail.com>
2022-09-05 23:12:29 +0300

    HW10 is done

---
+++ Даты/dates.js
@@ -0,0 +1,39 @@
+//Запросите у пользователя дату в формате ДД.ММ.ГГГГ.
+//Напишите программу, выводящую день недели по введённой дате.
+
+let date = prompt('Type a date: DD.MM.YYYY');
+
+(function getWeekDay() {
+    let [day, month, year] = date.split('.');
+    let inputDate = new Date(Number(year), Number(--month), Number(day));
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'];
+    alert(days[inputDate.getDay()]);
+}());
+
+//Написать программу, которая выводит в консоль количество минут, прошедшее с начала сегодняшнего дня.
+
+function secToday() {
+    const now = new Date();
+    const secPass = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+    console.log(secPass);
+}
+
+//*В двух переменных хранятся даты рождения двух пользователей в формате ДД.ММ.ГГГГ.
+//Написать программу, которая определяет более молодого пользователя.
+
+function yongestOne(user1, user2) {
+    let [day, month, year] = user1.split('.');
+    let [day2, month2, year2] = user2.split('.');
+
+    let date1 = new Date();
+    date1.setDate(day);
+    date1.setMonth(month - 1);
+    date1.setYear(year);
+
+    let date2 = new Date();
+    date2.setDate(day2);
+    date2.setMonth(month2 - 1);
+    date2.setYear(year2);
+
+    date1 < date2 ? alert('User1 is older than User2') : alert('User2 is older than User1');
+}
