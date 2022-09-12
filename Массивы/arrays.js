9ee29b35f02a0466981a2c33cb0e1e937116b6e6
Kramerale <aliceshoolz96@gmail.com>
2022-09-12 17:26:06 +0300

    Fix HW5

--- Массивы/arrays.js
+++ Массивы/arrays.js
@@ -4,20 +4,23 @@ let arr = [1,2,3,4,5,6,7,8,9,10];
 
 //1.Выведите в консоль сумму всех элементов массива.
 
-arr.forEach(function(el){
+(function sumOfNewArr() {
     let sum = 0;
-    sum += el;
+    arr.forEach(function(el){
+        sum += el;
+    });
     console.log(sum);
-});
+}(arr));
+
 
 //2.Создайте новый массив на основе исходного,
 //в котором каждый элемент будет вдвое больше элемента исходного массива с таким же индексом.
 //(a[1] = 3, b[1] = 6, где a — исходный массив, b — новый массив).
 
-let newArr = arr.map(function(el) {
-    let newValue = el * 2;
-    return newValue;
-});
+(function newArr() {
+    let newValue = [];
+    return newValue = arr.map((el) => el * 2);
+}(arr));
 
 //3.*Найдите и выведите в консоль наибольший и наименьший элементы исходного массива.
 
