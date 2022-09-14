c5306c72d10617abe6104f461640cad9e595aa9d
Kramerale <aliceshoolz96@gmail.com>
2022-09-15 01:23:24 +0300

    Fix HW Objects

--- Объекты/objects.js
+++ Объекты/objects.js
@@ -7,14 +7,19 @@ const user = {
 //1.Запросить у пользователя ввод числа.
 //Записать введенное значение в поле age объекта user.
 
-const input = prompt('Enter a number');
-user.age = input;
+(function addUserAge() {
+    const input = prompt('Enter a number');
+    user.age = input;
+}(user));
 
 //2.Создать копию объекта user с именем admin.
 //Добавить новому объекту поле role со значением ‘admin’.
 
-const admin = Object.assign({}, user);
-admin.role = 'admin';
+const admin = user;
+
+(function addAdminRole() {
+    admin.role = 'admin';
+}(admin));
 
 //*Записать все значения полей объекта admin в отдельные переменные.
 //Имена переменных должны совпадать с названиями полей.
